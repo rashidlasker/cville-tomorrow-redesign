6ddedd0ec600541db685d7ad52e3739c66e80da4
kle-github <khl3ej@virginia.edu>
2017-06-20 14:35:07 -0400

    changes to footer

--- footer.php
+++ footer.php
@@ -15,11 +15,45 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cville-tomorrow-theme' ), 'cville-tomorrow-theme', '<a href="https://automattic.com/">Underscores.me</a>' );
-			?>
+			<div class='footer-bg' style='height: 400px; width: 100%; background-color: #182e5a;'>
+				<div class="grid">
+				    <div class="grid__col grid__col--1-of-3 pages">
+					    <p class='footer-heading' style='width: 50%; padding-left: 50%'>Pages</p>
+						    <ul class='pages-items' style='color: white;'>
+							    <li style='padding-left: 30%'>Home</li>
+							    <li style='padding-left: 30%'>About Us</li>
+							    <li style='padding-left: 30%'>Jobs</li>
+							    <li style='padding-left: 30%'>Underwriting Opportunities</li>
+							    <li style='padding-left: 30%'>Privacy Policy</li>
+							    <li style='padding-left: 30%'>Our Writers</li>
+						    </ul>
+					</div>
+					<div class="grid__col grid__col--1-of-3 our-platform">
+					    <p class='footer-heading2' style="padding-left: 28%;">Our Platform</p>
+					    <div class="platform-list" style="height:250px">
+						    <ul class='platform-items' style='color: white;'>
+							    <li>Apps</li>
+							    <li>Tools</li>
+							    <li>RSS Feeds</li>
+							    <li>Meeting & Conference Room Info</li>
+							    <li>All Staff & Contributors</li>
+							    <span>© 2005-2017 Charlottesville Tomorrow. All Rights Reserved</span>
+						    </ul>
+						</div>
+					</div>
+					<div class="grid__col grid__col--1-of-3 contact">
+					    <p class='footer-heading3' style="padding-left: 25%;">Contact</p>
+					    	<ul class='contact-items' style='color: white;'>
+							    <li style='padding-top:-10px'>P.O. Box 1591</li>
+							    <li>100 5th Street NE</li>
+							    <li>Charlottesville, VA 22902</li>
+							    <li class="contact-number">434-260-1533</li>
+						    </ul>
+						    <img src="http://localhost/demo-copy/wp-content/uploads/2017/06/facebook.png">
+					</div>
+				</div>
+			</div>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
