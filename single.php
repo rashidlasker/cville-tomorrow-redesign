114199c177b9b9db7d8197bddf59121ad7484b26
kle-github <khl3ej@virginia.edu>
2017-06-21 15:19:06 -0400

    responsiveness optimization

--- single.php
+++ single.php
@@ -11,8 +11,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 		<?php
+
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_format() );
@@ -31,5 +31,6 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
+
 get_sidebar();
 get_footer();
